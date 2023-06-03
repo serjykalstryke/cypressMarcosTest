3e750f4f37f3ba9b974d77708ab44348f06214be
David Stinnett <davidstinnett@icloud.com>
2023-06-02 23:37:02 -0400

    application performs a basic test of starting an order, clicking the button to enter an address, types an address, clicks to order a large pepperoni magnifico, and verifies that the requested item is in the cart

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,31 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://www.marcos.com");
+    cy.get("#navigationStartOrder").click();
+    cy.get(".other-loc-cta").click();
+
+    cy.get("#addrClientInput").type("4020 Fine Creek Path");
+
+    cy.get("#cityClientInput").type("Powhatan");
+
+    cy.get("#stateClientInput").select("VA");
+
+    cy.get("#zipClientInput").type("23139");
+
+    //Site should verify that given address is not in a delivery area
+    cy.get("#addresslocateClient").click();
+
+    cy.contains("a", "Carryout").click();
+
+    cy.get(".recently-ordered-item")
+      .find(".panel-item-footer")
+      .find(".buttons-wrapper")
+      .find(".order-btn")
+      .eq(1)
+      .click();
+
+    cy.get("#order-tree-toggle-btn").click();
+
+    cy.get(".ng-tns-c187-9").contains("Lg. Pepperoni Magnifico");
+  });
+});
