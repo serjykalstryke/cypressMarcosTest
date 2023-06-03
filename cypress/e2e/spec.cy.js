8b70f03a0ecb78392cde0984ef8b6f46981139f8
David Stinnett <davidstinnett@icloud.com>
2023-06-02 23:39:29 -0400

    added comments to explain steps of test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,13 @@
+//* 1. Visit the Marcos website
+//* 2. Click the "Start Your Order" button
+//* 3. Click "other location" button
+//* 4. Enter address
+//* 5. Click "find address" button
+//* 6. Click "Carryout" button
+//* 7. Click button to order Lg. Pepperoni Magnifico
+//* 8. Click "View Order" button
+//* 9. Verify that the order is correct
+
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://www.marcos.com");
@@ -12,7 +22,7 @@ describe("template spec", () => {
 
     cy.get("#zipClientInput").type("23139");
 
-    //Site should verify that given address is not in a delivery area
+    //Site should verify that given address is not in a delivery area and only allow carryout
     cy.get("#addresslocateClient").click();
 
     cy.contains("a", "Carryout").click();
